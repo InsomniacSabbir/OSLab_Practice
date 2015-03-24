fb40dcf6a973805644b057fa77589106db533dcd
InsomniacSabbir <sabbiralam.cse@gmail.com>
2015-03-24 19:48:15 +0600

    Added current_log.sh and display_file_in_dir.sh files.

---
+++ ShellScript/current_log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -e "User name : $USER ( Login Name: $LOGNAME)"
+echo -e "Current Shell: $SHELL"
+echo -e "Home Dir: $HOME"
+echo -e "Your OS type: $OSTYPE"
+echo -e "PATH: $PATH"
+echo -e "Current Dir: `pwd`"
---
+++ ShellScript/display_file_in_dir.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in `ls -rS $1`
+do 
+    echo $i
+done
+
