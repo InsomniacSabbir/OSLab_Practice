416625059c5e993c1673c1a0b254c52b8b8937e4
InsomniacSabbir <sabbiralam.cse@gmail.com>
2015-03-24 21:03:45 +0600

    Edited the count_word_and_char.sh file.

--- ShellScript/count_word_and_char.sh
+++ ShellScript/count_word_and_char.sh
@@ -1,13 +1,4 @@
-#! /bin/sh
-w=0
-cc=0
-for i in `cat $1`
-do 
-   j=$i;
-   echo $j
-   w=`expr $w + 1`
-   c = `expr "$i" : "*"`
-   cc = `expr $cc + $c`
-done
-echo " no. of char " $cc
-echo " no. of word " $w
+#! /bin/bash
+echo "no. of char `wc -m $1`"
+echo "no. of word `wc -w $1`"
+echo "no. of line `wc -l $1`"
